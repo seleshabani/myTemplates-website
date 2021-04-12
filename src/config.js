4baa510d03e1f9bf77b7b1b2c168cd3c3af58455
seleshabani <seleshabani4@gmail.com>
2021-04-12 03:38:56 +0100

    update the api links

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_ROUTE = 'http://localhost:3500/api/';
-export const API_PUBLIC_ROUTE = 'http://localhost:3500/public/'
+export const API_ROUTE = 'https://thawing-river-56749.herokuapp.com/';
+export const API_PUBLIC_ROUTE = 'https://thawing-river-56749.herokuapp.com/public/'
